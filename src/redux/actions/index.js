afba83b82b39115c9e9783f38b07ee003b0f5ea6
KiranKumar-Ramanujam <kirankumar.ramanujam@gmail.com>
2023-05-19 16:38:50 +0800

    Multispectral_0.0.8

--- src/redux/actions/index.js
+++
@@ -1,5 +0,0 @@
-export * from './download_dropdown.action';
-export * from './switch.action';
-export * from './dropdown.action';
-export * from './trees.action';
-export * from './logindetails.action';
