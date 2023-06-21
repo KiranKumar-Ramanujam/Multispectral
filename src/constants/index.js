b61accc94322f6f7cdb4e79ccb2eb3175f2399b8
KiranKumar-Ramanujam <kirankumar.ramanujam@gmail.com>
2023-06-21 12:53:41 +0800

    Multispectral 1.0

--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-import colors from './colors';
-import device from './device';
-import fonts from './fonts';
-import gStyle from './globalStyles';
-import route from './route.constant';
-import utils from './utils';
-
-export {colors, device, fonts, gStyle, route, utils};
